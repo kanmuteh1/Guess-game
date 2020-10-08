86ffcbde650da6a7bc85d444cdc36c2a11b64e44
kanmuteh1 <jameskgayejr@gmail.com>
2020-10-08 22:16:10 +0000

    making a guess game

--- script.js
+++ script.js
@@ -1,44 +1,72 @@
-var action = document.querySelector("#action-key");
-
-action.addEventListener("click", function(e){
+var start = document.querySelector("#START");
+start.addEventListener("click", function(e){
     e.preventDefault();
-        var getGuess = document.querySelector("#take-input").value;
-        var randomNum = Math.floor(Math.random()*20)+1;
-        var textGuess = document.querySelector("#txt-display");
-        var chances = 4;
-    
-        while (chances > 0){
-        getGuess.innerHTML= "";
-        var guess = getGuess;
-        
-        if (chances == 0){
-            textGuess.innerHTML = "";
-            textGuess.innerHTML = "You lose"
-        }
+    // restart btn
+    start.innerHTML = "RESTART";
+    // setting body backgroundColor
+    var body = document.querySelector("body");
+    body.style.background = "Green";
+    // resetting guess value
+    var getGuess = document.querySelector("#take-input").value="";
+    // generating random number
+    var randomNum = Math.floor(Math.random()*100)+1;
+    // creating & displaying chances
+    var chances = 10;
+    var displayChances = document.querySelector("#chances");
+    displayChances.innerHTML="CHANCES: " + chances;
+    // guess game heading
+    var guessText = document.querySelector("#guess-text");
+    guessText.innerHTML = "PLEASE GUESS A NUMBER<br> 1 - 100";
+    // guess game results text
+    var textGuess = document.querySelector("#txt-display");
+    textGuess.innerHTML = "GUESS RESULT!!!";
+    //guess validator function
+    var action = document.querySelector("#action-key");
+    action.addEventListener("click", function(e){
+        e.preventDefault();
+        // take user's guess
+           var getGuess = document.querySelector("#take-input").value;
+        //previous guess function
+           var previousGuess = document.querySelector("#PREVIOUS-GUESSED");
+           previousGuess.addEventListener("click", function(e){
+            var guessedNum = document.querySelector("#GUESSED-NUM")
+            guessedNum.innerHTML = "Your previous guess was: " + getGuess; 
+           })
+           
+           var guess = Number(getGuess);
 
-        if(guess === randomNum){
-            textGuess.innerHTML = "";  
-            textGuess.innerHTML = "You win";
-            break;
-        }
-    
-        else if(guess > randomNum){
-            chances--;
-            textGuess.innerHTML = "";
-            textGuess.innerHTML = "Guess too high; you have " + chances + " guesses " + "left";
-        }
-    
-        else if (guess < randomNum){
-            chances--;
-            textGuess.innerHTML = "";
-            textGuess.innerHTML = "Guess too low; you have " + chances + " guesses " + "left"; 
-        }
+            if(guess === randomNum){
+                textGuess.innerHTML = "";  
+                textGuess.innerHTML = "YOU WIN!!!";
+            }
         
-        else if (isNaN(guess)){
-            chances--;
-            textGuess.innerHTML = "";
-            textGuess.innerHTML = "Guess is invalid; " + chances + " guesses " + "left";   
-        }
-        }
-    
+            else if(guess > randomNum){
+                chances--;
+                textGuess.innerHTML = "";
+                textGuess.innerHTML = "Guess too high";
+                displayChances.innerHTML="CHANCES: " + chances;
+            }
+        
+            else if (guess < randomNum){
+                chances--;
+                textGuess.innerHTML = "";
+                textGuess.innerHTML = "Guess too low";
+                displayChances.innerHTML="CHANCES: " + chances;
+            }
+            
+            else if (isNaN(guess)){
+                chances--;
+                textGuess.innerHTML = "";
+                textGuess.innerHTML = "Guess is invalid";
+                displayChances.innerHTML="CHANCES: " + chances;
+            }
+
+            if (chances <= 0){
+                textGuess.innerHTML = "";
+                textGuess.innerHTML ="You lose <br> the correct number is: " + randomNum;
+                body.style.background = "red";
+            }
+        
+        
+    })
 })
